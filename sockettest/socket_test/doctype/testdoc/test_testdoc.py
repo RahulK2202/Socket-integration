7a7c9fffd5149f43dd99603d6b0d66bf13384560
RahulK2202 <rahuljcet95@gmail.com>
2025-05-08 12:30:53 +0530

    testing licenseing mit

---
+++ sockettest/socket_test/doctype/testdoc/test_testdoc.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, rahul@anther.tech and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class Testtestdoc(FrappeTestCase):
+	pass
---
+++ sockettest/socket_test/doctype/testdoc/testdoc.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, rahul@anther.tech and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("testdoc", {
+// 	refresh(frm) {
+
+// 	},
+// });
